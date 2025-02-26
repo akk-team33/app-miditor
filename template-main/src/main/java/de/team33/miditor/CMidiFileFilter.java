9bed4e51b01da50ce4f664c5d2054fc6458c1914
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-02-26 15:29:42 +0100

    added some de.team33.miditor.*

---
+++ template-main/src/main/java/de/team33/miditor/CMidiFileFilter.java
@@ -0,0 +1,35 @@
+package de.team33.miditor;
+
+import java.io.File;
+import javax.swing.filechooser.FileFilter;
+
+public class CMidiFileFilter extends FileFilter {
+    private static final String[] EXTENSION = new String[]{"mid", "midi"};
+    private static final String DESCRIPTION = "MIDI Standard File (*.mid, *.midi)";
+
+    public CMidiFileFilter() {
+    }
+
+    public boolean accept(File f) {
+        if (f.isDirectory()) {
+            return true;
+        } else {
+            String fExt = f.getName().replaceAll("[^.]*[.]", "");
+            String[] var6;
+            int var5 = (var6 = EXTENSION).length;
+
+            for(int var4 = 0; var4 < var5; ++var4) {
+                String mExt = var6[var4];
+                if (fExt.equalsIgnoreCase(mExt)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public String getDescription() {
+        return "MIDI Standard File (*.mid, *.midi)";
+    }
+}
