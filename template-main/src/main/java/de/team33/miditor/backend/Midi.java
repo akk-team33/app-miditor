c954f49f3130543ec2151ed462e267c71b43aaa9
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-03-09 06:17:23 +0100

    added Midi.Message.Type

--- template-main/src/main/java/de/team33/miditor/backend/Midi.java
+++ template-main/src/main/java/de/team33/miditor/backend/Midi.java
@@ -1,6 +1,98 @@
 package de.team33.miditor.backend;
 
+import de.team33.patterns.decision.carpo.Variety;
+
+import javax.sound.midi.MidiMessage;
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+@SuppressWarnings({"InterfaceNeverImplemented", "MarkerInterface", "unused"})
 interface Midi {
+
+    interface Message {
+
+        enum Group {
+
+            CHANEL(0xF0),
+            SYSTEM(0xFF);
+
+            private final int statusMask;
+
+            Group(final int statusMask) {
+                this.statusMask = statusMask;
+            }
+        }
+
+        enum Type {
+
+            NOTE_OFF(Group.CHANEL, 0x80, 3),
+            NOTE_ON(Group.CHANEL, 0x90, 3),
+            P_PRESS(Group.CHANEL, 0xA0, 3),
+            CONTROL(Group.CHANEL, 0xB0, 3),
+            PROGRAM(Group.CHANEL, 0xC0, 2),
+            C_PRESS(Group.CHANEL, 0xD0, 2),
+            PITCH_BEND(Group.CHANEL, 0xE0, 3),
+            SYS_EX(Group.SYSTEM, 0xF0, 3, Integer.MAX_VALUE),
+            MTC(Group.SYSTEM, 0xF1, 2),
+            SONG_PP(Group.SYSTEM, 0xF2, 3),
+            SONG_SEL(Group.SYSTEM, 0xF3, 2),
+            F4(Group.SYSTEM, 0xF4, 1, 3),
+            F5(Group.SYSTEM, 0xF5, 1, 3),
+            TUNE_REQ(Group.SYSTEM, 0xF6, 1),
+            EOX(Group.SYSTEM, 0xF7, 1),
+            CLOCK(Group.SYSTEM, 0xF8, 1),
+            F9(Group.SYSTEM, 0xF9, 1, 3),
+            START(Group.SYSTEM, 0xFA, 1),
+            CONTINUE(Group.SYSTEM, 0xFB, 1),
+            STOP(Group.SYSTEM, 0xFC, 1),
+            FD(Group.SYSTEM, 0xFD, 1),
+            META(Group.SYSTEM, 0xFF, 3, 130);
+
+            private static final String ILLEGAL_MESSAGE = "Not a valid message of type %s:%n" +
+                                                          "    status: %02Xh%n" +
+                                                          "    length: %d%n" +
+                                                          "    bytes:  %s%n";
+
+            private final Predicate<byte[]> identificator;
+            private final Variety<byte[]> validator;
+
+            @SuppressWarnings("ParameterNameDiffersFromOverriddenParameter")
+            Type(final Group group, final int status, final int length) {
+                this(group, status, length, length);
+            }
+
+            Type(final Group group, final int status, final int minLength, final int maxLength) {
+                final Predicate<byte[]> isStatus = bytes -> (bytes[0] & group.statusMask) == status;
+                final Predicate<byte[]> isLength;
+                if (minLength == maxLength) {
+                    isLength = bytes -> bytes.length == minLength;
+                } else {
+                    isLength = bytes -> (minLength <= bytes.length) && (bytes.length <= maxLength);
+                }
+                this.identificator = isStatus;
+                this.validator = Variety.joined(isStatus, isLength);
+            }
+
+            final boolean isTypeOf(final MidiMessage message) {
+                return identificator.test(message.getMessage());
+            }
+
+            final boolean isValid(final MidiMessage message) {
+                return 0b11 == validator.apply(message.getMessage());
+            }
+
+            final MidiMessage valid(final MidiMessage message) {
+                if (isValid(message)) {
+                    return message;
+                } else {
+                    final byte[] bytes = message.getMessage();
+                    final String text = ILLEGAL_MESSAGE.formatted(name(), bytes[0] & 0xff, bytes.length,
+                                                                  Arrays.toString(bytes));
+                    throw new IllegalArgumentException(text);
+                }
+            }
+        }
+    }
 
     interface MetaMessage {
 
---
+++ template-main/src/test/java/de/team33/miditor/backend/MidiMessageTypeTest.java
@@ -0,0 +1,70 @@
+package de.team33.miditor.backend;
+
+import de.team33.miditor.backend.Midi.Message.Type;
+import de.team33.miditor.testing.AnyMessage;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import javax.sound.midi.MidiMessage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MidiMessageTypeTest {
+
+    @ParameterizedTest
+    @EnumSource
+    void isTypeOf(final Case given) {
+        assertEquals(given.isTypeOf, given.type.isTypeOf(given.midiMessage));
+    }
+
+    @ParameterizedTest
+    @EnumSource
+    void isValid(final Case given) {
+        assertEquals(given.isValid, given.type.isValid(given.midiMessage));
+    }
+
+    @ParameterizedTest
+    @EnumSource
+    void valid(final Case given) {
+        try {
+            final MidiMessage result = given.type.valid(given.midiMessage);
+            assertTrue(given.isValid, () -> "expected to fail - but was " + result);
+        } catch (final IllegalArgumentException e) {
+            // e.printStackTrace();
+            assertFalse(given.isValid, () -> "NOT expected to fail - but was " + e);
+            assertTrue(e.getMessage().contains(given.type.name()));
+        }
+    }
+
+    @SuppressWarnings("unused")
+    enum Case {
+
+        NOTE_OFF(Type.NOTE_OFF, true, true, 0x80, 63, 63),
+        NOTE_OFF_FF(Type.NOTE_OFF, false, false, 0x90, 63, 63),
+        NOTE_OFF_TF(Type.NOTE_OFF, true, false, 0x80, 63, 63, 63),
+        NOTE_ON(Type.NOTE_ON, true, true, 0x90, 63, 63),
+        NOTE_ON_FF(Type.NOTE_ON, false, false, 0x80, 63, 63),
+        NOTE_ON_TF(Type.NOTE_ON, true, false, 0x90, 63),
+        P_PRESS(Type.P_PRESS, true, true, 0xA0, 63, 63),
+        CONTROL(Type.CONTROL, true, true, 0xB0, 63, 63),
+        PROGRAM(Type.PROGRAM, true, true, 0xC0, 63),
+        SYS_EX(Type.SYS_EX, true, true, 0xF0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9),
+        EOX(Type.EOX, true, true, 0xF7),
+        META(Type.META, true, true, 0xFF, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9),
+        META_TF(Type.META, true, false, 0xFF);
+
+        private final Type type;
+        private final boolean isTypeOf;
+        private final boolean isValid;
+        private final MidiMessage midiMessage;
+
+        Case(final Type type, boolean isTypeOf, boolean isValid, final int... values) {
+            this.type = type;
+            this.isTypeOf = isTypeOf;
+            this.isValid = isValid;
+            this.midiMessage = AnyMessage.of(values);
+        }
+    }
+}
