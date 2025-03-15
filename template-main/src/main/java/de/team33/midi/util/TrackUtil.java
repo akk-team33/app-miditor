340c0d885dd1bf87dfe3566d66d0798dcda05efc
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-03-15 21:45:07 +0100

    refined TrackUtil

--- template-main/src/main/java/de/team33/midi/util/TrackUtil.java
+++ template-main/src/main/java/de/team33/midi/util/TrackUtil.java
@@ -3,41 +3,22 @@ package de.team33.midi.util;
 import de.team33.midi.MidiTrack;
 
 import javax.sound.midi.MidiEvent;
-import java.util.Collection;
 import java.util.List;
-import java.util.Vector;
+import java.util.stream.IntStream;
 
-public class TrackUtil {
-    public TrackUtil() {
+public final class TrackUtil {
+
+    private TrackUtil() {
     }
 
-    public static void add(MidiTrack track, Collection<MidiEvent> events) {
-        MidiEvent[] array = (MidiEvent[]) events.toArray(new MidiEvent[events.size()]);
-        track.add(array);
+    private static List<MidiEvent> getEvents(final MidiTrack track, final int[] selection) {
+        return IntStream.of(selection)
+                        .filter(index -> (0 <= index) && (index < track.size()))
+                        .mapToObj(track::get)
+                        .toList();
     }
 
-    public static List<MidiEvent> getEvents(MidiTrack track, int[] selection) {
-        List<MidiEvent> ret = new Vector(selection.length);
-        int size = track.size();
-        int[] var7 = selection;
-        int var6 = selection.length;
-
-        for (int var5 = 0; var5 < var6; ++var5) {
-            int index = var7[var5];
-            if (index >= 0 && index < size) {
-                ret.add(track.get(index));
-            }
-        }
-
-        return ret;
-    }
-
-    public static void remove(MidiTrack track, Collection<MidiEvent> events) {
-        MidiEvent[] array = (MidiEvent[]) events.toArray(new MidiEvent[events.size()]);
-        track.remove(array);
-    }
-
-    public static void remove(MidiTrack track, int[] selection) {
-        remove(track, (Collection) getEvents(track, selection));
+    public static void remove(final MidiTrack track, final int[] selection) {
+        track.remove(getEvents(track, selection));
     }
 }
