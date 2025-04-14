47593e723b4943b3e1d965ecaa1576aab2c96376
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-04-14 15:53:53 +0200

    [ui-02] refined new XSpinner, used in TempoControl (not ready)

--- miditor-main/src/main/java/de/team33/miditor/ui/XSpinner.java
+++ miditor-main/src/main/java/de/team33/miditor/ui/XSpinner.java
@@ -7,6 +7,12 @@ import de.team33.sphinx.alpha.visual.JLabels;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ComponentEvent;
+import java.awt.event.FocusEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BiConsumer;
@@ -14,26 +20,32 @@ import java.util.function.Consumer;
 
 public final class XSpinner extends JLabel {
 
+    private final String format;
+    private final List<Consumer<Integer>> audience = new LinkedList<>();
     private final AtomicInteger value = new AtomicInteger();
     @SuppressWarnings("SpellCheckingInspection")
     private final AtomicBoolean hilited = new AtomicBoolean();
     @SuppressWarnings("FieldHasSetterButNoGetter")
     private final AtomicBoolean entered = new AtomicBoolean();
-    @SuppressWarnings("FieldHasSetterButNoGetter")
     private final AtomicBoolean focussed = new AtomicBoolean();
     private boolean oldOpaque = isOpaque();
     private Color oldForeground = getForeground();
     private Color oldBackground = getBackground();
 
-    public static Builder builder() {
-        return new Builder().setCursor(new Cursor(Cursor.N_RESIZE_CURSOR))
-                            .setFocusable(true)
-                            .on(Event.MOUSE_ENTERED, cast((spinner, event) -> spinner.setEntered(true)))
-                            .on(Event.MOUSE_EXITED, cast((spinner, event) -> spinner.setEntered(false)))
-                            .on(Event.MOUSE_PRESSED, cast((spinner, event) -> spinner.requestFocus()))
-                            //.on(Event.MOUSE_WHEEL_MOVED, XSpinner::onMouseWheelMoved)
-                            .on(Event.FOCUS_GAINED, cast((spinner, event) -> spinner.setFocussed(true)))
-                            .on(Event.FOCUS_LOST, cast((spinner, event) -> spinner.setFocussed(false)));
+    private XSpinner(final String format) {
+        this.format = format;
+    }
+
+    public static Builder builder(final String format) {
+        return new Builder(format).setCursor(new Cursor(Cursor.N_RESIZE_CURSOR))
+                                  .setFocusable(true)
+                                  .on(Event.MOUSE_ENTERED, cast(XSpinner::onMouseEntered))
+                                  .on(Event.MOUSE_EXITED, cast(XSpinner::onMouseExited))
+                                  .on(Event.MOUSE_PRESSED, cast(XSpinner::onMousePressed))
+                                  .on(Event.MOUSE_WHEEL_MOVED, cast(XSpinner::onMouseWheelMoved))
+                                  .on(Event.KEY_PRESSED, cast(XSpinner::onKeyPressed))
+                                  .on(Event.FOCUS_GAINED, cast(XSpinner::onFocusGained))
+                                  .on(Event.FOCUS_LOST, cast(XSpinner::onFocusLost));
     }
 
     private static <E extends ComponentEvent> Consumer<E> cast(final BiConsumer<XSpinner, E> consumer) {
@@ -46,6 +58,41 @@ public final class XSpinner extends JLabel {
         };
     }
 
+    @SuppressWarnings("MagicNumber")
+    private void onKeyPressed(final KeyEvent event) {
+        // System.out.printf("%d%n", event.getKeyCode());
+        switch (event.getKeyCode()) {
+        case 38, 39, 107, 521 -> setValue(getValue() + 1);
+        case 37, 40, 45, 109 -> setValue(getValue() - 1);
+        }
+    }
+
+    private void onFocusLost(final FocusEvent event) {
+        focussed.set(false);
+        setHilited();
+    }
+
+    private void onFocusGained(final FocusEvent event) {
+        focussed.set(true);
+        setHilited();
+    }
+
+    private void onMouseWheelMoved(final MouseWheelEvent event) {
+        setValue(getValue() - event.getWheelRotation());
+    }
+
+    private void onMousePressed(final MouseEvent event) {
+        requestFocus();
+    }
+
+    private void onMouseExited(final MouseEvent event) {
+        setEntered(false);
+    }
+
+    private void onMouseEntered(final ComponentEvent event) {
+        setEntered(true);
+    }
+
     private void setEntered(final boolean entered) {
         this.entered.set(entered);
         setHilited();
@@ -53,7 +100,7 @@ public final class XSpinner extends JLabel {
 
     private void setHilited() {
         final boolean newHilited = (entered.get() || focussed.get());
-        if (newHilited  != hilited.get()) {
+        if (newHilited != hilited.get()) {
             hilited.set(newHilited);
             if (newHilited) {
                 oldOpaque = isOpaque();
@@ -70,32 +117,36 @@ public final class XSpinner extends JLabel {
         }
     }
 
-    private void setFocussed(final boolean focussed) {
-        this.focussed.set(focussed);
-        setHilited();
-    }
-
+    @SuppressWarnings("WeakerAccess")
     public final int getValue() {
         return value.get();
     }
 
     public final void setValue(final int value) {
-        this.value.set(value);
-        setText(String.format("%03d", value));
+        final int oldValue = getValue();
+        if (value != oldValue && this.value.compareAndSet(oldValue, value)) {
+            setText(String.format(format, value));
+            audience.forEach(listener -> listener.accept(value));
+        }
     }
 
+    @SuppressWarnings("unused")
     @FunctionalInterface
     public interface Setup<S extends Setup<S>> extends JLabels.Setup<XSpinner, S> {
 
         default S setValue(final int value) {
             return setup(xSpinner -> xSpinner.setValue(value));
         }
+
+        default S onSetValue(final Consumer<Integer> listener) {
+            return setup(xSpinner -> xSpinner.audience.add(listener));
+        }
     }
 
     public static final class Builder extends LateBuilder<XSpinner, Builder> implements Setup<Builder> {
 
-        private Builder() {
-            super(XSpinner::new, Builder.class);
+        private Builder(final String format) {
+            super(() -> new XSpinner(format), Builder.class);
         }
     }
 }
--- miditor-main/src/main/java/de/team33/miditor/ui/player/TempoControl.java
+++ miditor-main/src/main/java/de/team33/miditor/ui/player/TempoControl.java
@@ -2,7 +2,7 @@ package de.team33.miditor.ui.player;
 
 import de.team33.midi.Player;
 import de.team33.miditor.ui.Basics;
-import de.team33.swing.XSpinner;
+import de.team33.miditor.ui.XSpinner;
 
 import javax.swing.*;
 import java.awt.*;
@@ -25,51 +25,14 @@ public abstract class TempoControl {
     protected abstract Player getPlayer();
 
     private JLabel label() {
-        return de.team33.miditor.ui.XSpinner
-                .builder()
-                .setBorder(createCompoundBorder(createLineBorder(Color.GRAY),
-                                                createEmptyBorder(1, 4, 1, 4)))
-                .setup(xSpinner -> Basics.setFont(xSpinner, Font.BOLD, 18))
-                .setup(xSpinner -> getPlayer().registry()
-                                              .add(Player.Channel.SET_TEMPO, xSpinner::setValue))
-                .build();
-    }
-
-    private class LABEL extends XSpinner {
-        private static final long serialVersionUID = -5125403795254334049L;
-
-        public LABEL() {
-            setBorder(createCompoundBorder(createLineBorder(Color.GRAY), createEmptyBorder(1, 4, 1, 4)));
-            setFont(new Font(getFont().getName(), 1, 18));
-            getPlayer().registry().add(Player.Channel.SET_TEMPO, this::onSetTempo);
-        }
-
-        protected final void decrease(final int exponent) {
-            updateTempo(-1, exponent);
-        }
-
-        protected final void increase(final int exponent) {
-            updateTempo(1, exponent);
-        }
-
-        protected final void maximize() {
-            getPlayer().setTempo(180);
-        }
-
-        protected final void minimize() {
-            getPlayer().setTempo(60);
-        }
-
-        private void updateTempo(int delta, int exponent) {
-            while (exponent-- > 0) {
-                delta *= 10;
-            }
-
-            getPlayer().setTempo(getPlayer().getTempo() + delta);
-        }
-
-        public final void onSetTempo(final int tempo) {
-            setText(String.format("%03d", tempo));
-        }
+        //noinspection MagicNumber
+        return XSpinner.builder("%03d")
+                       .setBorder(createCompoundBorder(createLineBorder(Color.GRAY),
+                                                       createEmptyBorder(1, 4, 1, 4)))
+                       .setup(xSpinner -> Basics.setFont(xSpinner, Font.BOLD, 18))
+                       .setup(xSpinner -> getPlayer().registry()
+                                                     .add(Player.Channel.SET_TEMPO, xSpinner::setValue))
+                       .onSetValue(value -> getPlayer().setTempo(value))
+                       .build();
     }
 }
